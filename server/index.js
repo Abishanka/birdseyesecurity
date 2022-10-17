3903878ceac920fd1a731ce10ed41bbe46fa63f5
abishanka <abishanka@gmail.com>
2022-10-16 23:05:54 -0400

    Updated backend with SQL

--- server/index.js
+++ server/index.js
@@ -11,10 +11,23 @@ app.listen(3001, () => {
 app.post('/insert_row', function(req,res){
     //anytime new row button is clicked, execute
     //send some response...
-    
-})
+    db.one(`INSERT INTO <tablename> VALUES (${req.query.alarmTime}, ${req.query.alarmPoint}, ${req.query.alarmTag}, ${req.query.tagNotes}, ${req.query.otherNotes}, ${req.query.workOrder}, ${req.query.extraInfo})`)
+        .then(() => {
+            db.one(`SELECT * FROM <tablename> WHERE alarmPoint="${req.query.alarmPoint}"`)
+                .then((data) => {
+                    res.send(data)
+                })
+        })
+});
 
 app.post('/edit_row', function(req,res){
     //anytime save button is clicked (after edit button), exeucte
     //send some response...
+    db.one(`UPDATE <tablename> SET (alarmTime=\'${req.query.alarmTime}\', alarmPoint=\'${req.query.alarmPoint}\', alarmTag=\'${req.query.alarmTag}\', tagNotes=\'${req.query.tagNotes}\', otherNotes=\'${req.query.otherNotes}\', workOrder=\'${req.query.workOrder}\', extraInfo=\'${req.query.extraInfo}\')`)
+        .then(() => {
+            db.one(`SELECT * FROM <tablename> WHERE alarmPoint="${req.query.alarmPoint}"`)
+                .then((data) => {
+                    res.send(data)
+                })
+        })
 })

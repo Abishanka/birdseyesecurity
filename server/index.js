2b5734c8affafb4c85dc084ecd568d2d40549aa7
abishanka <abishanka@gmail.com>
2022-10-16 22:32:35 -0400

    Updated backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 //we're going to require the express middleware (express dependency) 
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const pgp = require('pg-promise')
+const db = pgp('postgres://username:password@host:port/database')
 
 app.listen(3001, () => {
     console.log("running on port 3001");
+})
+
+app.post('/insert_row', function(req,res){
+    //anytime new row button is clicked, execute
+    //send some response...
+    
+})
+
+app.post('/edit_row', function(req,res){
+    //anytime save button is clicked (after edit button), exeucte
+    //send some response...
 })

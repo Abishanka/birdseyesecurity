fc07ea24f76ccbbfce6bc29028a91ca2dcb6d4ee
gracelhu <gracehu2003@gmail.com>
2022-10-20 23:34:58 -0400

    used bootstrap components for Edit Alarm Data pg.

--- client/app/src/pages/EditAlarmData.js
+++ client/app/src/pages/EditAlarmData.js
@@ -1,27 +1,31 @@
+import * as boot from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function EditAlarmData() {
     return (
         <>
             <h1>Edit Alarm Data</h1>
-            <form class = "edit">
-                <label>Data type: </label>
-                <select>
-                    <option value="bldg name">bldg name</option>
-                    <option value="bldg #">bldg #</option>
-                    <option value="alarm point">alarm point</option>
-                    <option value="alarm name + #">alarm name + #</option>
-                </select>
-                <label>Action: </label>
-                <select>
-                    <option value="add">add to database</option>
-                    <option value="delete">delete from database</option>
-                </select>
-                <input
-                    type="text"
-                    className="inputField"
-                    required
-                />
-                <button>submit</button>
-            </form>
+            {/* Change state when you click an item from menu, right now it does nothing */}
+            <boot.DropdownButton size="md" title = "Action" className = "action">
+                        <boot.Dropdown.Item>add to database</boot.Dropdown.Item>
+                        <boot.Dropdown.Item>delete from database</boot.Dropdown.Item>
+            </boot.DropdownButton>
+            <boot.Container>
+                <boot.Form>
+                    <boot.Form.Group class="form-group">
+                        <boot.Form.Label>Alarm Point: </boot.Form.Label>
+                        <boot.Form.Control name="alarmPoint" placeholder="Alarm Point"></boot.Form.Control>
+                        <boot.Form.Label>Bldg. Name/Bldg.#: </boot.Form.Label>
+                        <boot.Form.Control name="bldgNameAndNumber" placeholder="Bldg. Name/Bldg.#"></boot.Form.Control>
+                        <boot.Form.Label>Alarm Name/#: </boot.Form.Label>
+                        <boot.Form.Control name="alarmNameAndNumber" placeholder="Alarm Name/#"></boot.Form.Control>
+                        <boot.Form.Label>Sensor Status: </boot.Form.Label>
+                        <boot.Form.Control name="sensorStatus" placeholder="Sensor Status"></boot.Form.Control>
+                    </boot.Form.Group>
+                </boot.Form>
+            </boot.Container>
+            {/*TO DO: Send info to back-end*/}
+            <boot.Button>Submit</boot.Button>
         </>
     )
 }
